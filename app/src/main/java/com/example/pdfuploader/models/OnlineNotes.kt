66663f591500c0aa29a620f3bf589780b1df26c9
KingsMunene <munenekings2@gmail.com>
2023-08-19 17:16:32 +0300

    Change the default values for the data class from null to empty string

--- app/src/main/java/com/example/pdfuploader/models/OnlineNotes.kt
+++ app/src/main/java/com/example/pdfuploader/models/OnlineNotes.kt
@@ -3,6 +3,6 @@ package com.example.pdfuploader.models
 import android.net.Uri
 import java.net.URL
 
-data class OnlineNotes(var name: String? = null, var unit: String? = null, var category: String? = null)
+data class OnlineNotes(var name: String = "", var unit: String = "", var category: String = "")
 
 data class Course(var courseName: String? = null, var onLineUnits: List<OnlineNotes>? = null)
